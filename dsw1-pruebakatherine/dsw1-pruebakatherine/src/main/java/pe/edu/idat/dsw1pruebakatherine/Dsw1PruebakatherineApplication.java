092e1f1f2e6b067753c40a7b042f24ba14f33480
katherineloayza <katherineloayza8@gmail.com>
2023-06-08 21:25:09 -0500

    Proyecto java recien inicializado

---
+++ dsw1-pruebakatherine/dsw1-pruebakatherine/src/main/java/pe/edu/idat/dsw1pruebakatherine/Dsw1PruebakatherineApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1pruebakatherine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1PruebakatherineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1PruebakatherineApplication.class, args);
+	}
+
+}
---
+++ dsw1-pruebakatherine/dsw1-pruebakatherine/src/test/java/pe/edu/idat/dsw1pruebakatherine/Dsw1PruebakatherineApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1pruebakatherine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1PruebakatherineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
